d45fcc82fe103b94bff65478a16703c2066cf53c
unknown <amitbiswas9282@gmail.com>
2024-07-26 05:03:37 +0600

    This is Problem commit

--- components/StairTransition.jsx
+++ components/StairTransition.jsx
@@ -1,18 +1,26 @@
 "use client";
 
-import { AnimatePresence } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import { useParams } from "next/navigation";
 import Stairs from "./Stairs";
 
 const StairTransition = () => {
-  const paramsName = useParams();
+  const pathname = useParams();
   return (
     <>
       <AnimatePresence made="wait">
-        <div key={paramsName}>
+        <div key={pathname}>
           <div className="h-screen w-screen fixed top-0 left-0 right-0 z-40 pointer-events-none flex">
             <Stairs />
           </div>
+          <motion.div
+            className="w-screen h-screen fixed bg-primary top-0 pointer-events-none"
+            initial={{ opacity: 1 }}
+            animate={{
+              opacity: 0,
+              transition: { delay: 1, duration: 0.4, ease: "easeInOut" },
+            }}
+          />
         </div>
       </AnimatePresence>
     </>
--- components/Stairs.jsx
+++ components/Stairs.jsx
@@ -1,7 +1,49 @@
-import React from "react";
+import { motion } from "framer-motion";
+
+const stairsAnimation = {
+  initial: {
+    top: "0%",
+  },
+  animate: {
+    top: "100%",
+  },
+  exit: {
+    top: ["100%", "0%"],
+  },
+};
+
+// Calculate the reverse index for staggered delay
+const reverseIndex = (index) => {
+  const totalSteps = 6; //number of stair
+  return totalSteps - index - 1;
+};
 
 const Stairs = () => {
-  return <div>Stairs</div>;
+  return (
+    <>
+      {/* Render 6 motion divs each representing a step of the stairs
+         Each div will have some animation defined by the stairsAnimation object.
+         The delay for each div is calculated dynamically based on its reversed index,
+         creating a staggered effect with decreasing delay for each subsequent step. */}
+      {[...Array(6)].map((_, index) => {
+        return (
+          <motion.div
+            key={index}
+            variants={stairsAnimation}
+            initial="initial"
+            animate="animate"
+            exit="exit"
+            transition={{
+              duration: 0.4,
+              ease: "easeInOut",
+              delay: reverseIndex(index) * 0.1,
+            }}
+            className="h-full w-full bg-white relative"
+          />
+        );
+      })}
+    </>
+  );
 };
 
 export default Stairs;
