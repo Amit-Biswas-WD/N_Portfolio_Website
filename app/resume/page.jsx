a869aaf30136dc4bf168cf7e8edbd99c06be6e23
unknown <amitbiswas9282@gmail.com>
2024-07-28 01:15:02 +0600

    This is Resume

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -1,3 +1,45 @@
+"use client";
+
+import {
+  FaHtml5,
+  FaCss,
+  FaJs,
+  FaReact,
+  FaFigma,
+  FaNodeJs,
+} from "react-icons/fa";
+
+import { SiTailwindcss, SiNextdotjs } from "react-icons/si";
+
+const about = {
+  title: "About Me",
+  description:
+    "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate",
+  info: [
+    { fieldName: "Name", FieldValue: "Amit Biswas" },
+    { fieldName: "Phone", FieldValue: "+88 01883478528" },
+    { fieldName: "Experience", FieldValue: "1+ Years" },
+    { fieldName: "Nationality", FieldValue: "Bangladeshi" },
+    { fieldName: "Email", FieldValue: "amitbiswas9282@gmail.com" },
+    { fieldName: "Web Development", FieldValue: "Available" },
+    { fieldName: "Language", FieldValue: "English, Bangla" },
+  ],
+};
+
+const experience = {
+  icon: "/",
+  title: "My experience",
+  description:
+    "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate",
+  items: [
+    {
+      company: "",
+      position: "Front-end Web Developer / MERN Stack Developer",
+      duration: "2023 - present",
+    },
+  ],
+};
+
 const Resume = () => {
   return <div>Resume page</div>;
 };
