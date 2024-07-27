f6a7de56ed8cae89455397eb81e9a41f1cfd3062
unknown <amitbiswas9282@gmail.com>
2024-07-28 02:45:54 +0600

    This is resume title description complete

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -139,16 +139,36 @@ const Resume = () => {
           </TabsList>
           <div className="min-h-[70vh] w-full">
             <TabsContent value="experience" className="w-full">
-              Experience
+              <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                <h3 className="text-4xl font-bold">{experience.title}</h3>
+                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
+                  {experience.description}
+                </p>
+              </div>
             </TabsContent>
             <TabsContent value="education" className="w-full">
-              Education
+              <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                <h3 className="text-4xl font-bold">{education.title}</h3>
+                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
+                  {education.description}
+                </p>
+              </div>
             </TabsContent>
             <TabsContent value="skills" className="w-full">
-              Skills
+              <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                <h3 className="text-4xl font-bold">{skills.title}</h3>
+                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
+                  {skills.description}
+                </p>
+              </div>
             </TabsContent>
             <TabsContent value="about" className="w-full">
-              About me
+              <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                <h3 className="text-4xl font-bold">{about.title}</h3>
+                <p className="max-w-[600px] text-white/60 mx-auto xl:mx-0">
+                  {about.description}
+                </p>
+              </div>
             </TabsContent>
           </div>
         </Tabs>
