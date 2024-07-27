f14152938afd6de2f39d8db74801dd2c366a6294
unknown <amitbiswas9282@gmail.com>
2024-07-28 00:43:38 +0600

    This is grop

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -1,59 +1,74 @@
 "use client";
 
 import { BsArrowDownRight } from "react-icons/bs";
+import { motion } from "framer-motion";
+import Link from "next/link";
 
-const services = [
+const service = [
   {
     num: "01",
     title: "Web Development",
     description:
       "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia autem cum!",
+    href: "/web-development",
   },
   {
     num: "02",
     title: "UI/UX Design",
     description:
       "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia autem cum!",
+    href: "/ui-ux-design",
   },
   {
     num: "03",
     title: "Logo Design",
     description:
       "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia autem cum!",
+    href: "/logo-design",
   },
   {
     num: "04",
     title: "SEO",
     description:
       "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia autem cum!",
+    href: "/seo",
   },
 ];
-
-import { motion } from "framer-motion";
-import Link from "next/link";
 
 const Services = () => {
   return (
     <section className="min-h-[80vh] flex flex-col justify-center py-12 xl:py-0">
       <div className="container mx-auto">
         <motion.div
+          className="grid grid-cols-1 xl:grid-cols-2 gap-[60px]"
           initial={{ opacity: 0 }}
           animate={{
             opacity: 1,
             transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
           }}
         >
-          {services.map((item, index) => {
+          {service.map((item, index) => {
             return (
-              <div key={index}>
-                <div>
-                  <div>{services.num}</div>
-                  <Link href={services.href}>
-                    <BsArrowDownRight />
+              <div
+                key={index}
+                className="flex-1 flex flex-col justify-center gap-6 group"
+              >
+                <div className="w-full flex justify-between items-center">
+                  <div className="text-5xl font-semibold text-outline text-transparent group-hover:text-outline-hover transition-all duration-500">
+                    {item.num}
+                  </div>
+                  <Link
+                    href={item.href}
+                    className="w-[70px] h-[70px] rounded-full bg-white group-hover:bg-accent transition-all duration-500 flex justify-center items-center hover:-rotate-45"
+                  >
+                    <BsArrowDownRight className="text-primary text-3xl" />
                   </Link>
                 </div>
-                <h2>{item.title}</h2>
-                <p></p>
+                <h2 className="text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500">
+                  {item.title}
+                </h2>
+                <p className="text-white/60">{item.description}</p>
+                <div className="border-b text-white/20 w-full"></div>
               </div>
             );
           })}
