67131ccc6ada1cfbb6b978c7546db24c2f5cce53
unknown <amitbiswas9282@gmail.com>
2024-07-28 01:34:06 +0600

    This is skills

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -8,6 +8,7 @@ import {
   FaFigma,
   FaNodeJs,
 } from "react-icons/fa";
+import { DiMongodb } from "react-icons/di";
 
 import { SiTailwindcss, SiNextdotjs } from "react-icons/si";
 
@@ -36,6 +37,69 @@ const experience = {
       company: "",
       position: "Front-end Web Developer / MERN Stack Developer",
       duration: "2023 - present",
+    },
+  ],
+};
+
+const education = {
+  icon: "/",
+  title: "My Education",
+  description:
+    "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate",
+  items: [
+    {
+      institution: "Nation University",
+      dergee: "Bachelors in Arts",
+      duration: "2024 - present",
+    },
+    {
+      institution: "Programming Hero",
+      dergee: "Front-end Web Developer / MERN Stack Developer",
+      duration: "2023/12/26 - 2024/06/30",
+    },
+  ],
+};
+
+const skills = {
+  title: "My Skills",
+  description:
+    "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate",
+  skillList: [
+    {
+      icon: <FaHtml5 />,
+      name: "Html 5",
+    },
+    {
+      icon: <FaCss />,
+      name: "CSS 3",
+    },
+    {
+      icon: <SiTailwindcss />,
+      name: "Tailwindcss",
+    },
+    {
+      icon: <FaJs />,
+      name: "JavaScript",
+    },
+    {
+      icon: <FaReact />,
+      name: "React JS",
+    },
+    {
+      icon: <FaFigma />,
+      name: "Fima",
+    },
+    {
+      icon: <FaNodeJs />,
+      name: "NodeJs",
+    },
+    {
+      icon: <SiNextdotjs />,
+      name: "NextJS",
+    },
+    {
+      icon: <DiMongodb />,
+      name: "MongoDB",
     },
   ],
 };
