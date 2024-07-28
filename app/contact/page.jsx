27eb842746c23cbe46216974cb8fa3cb4a343bbe
unknown <amitbiswas9282@gmail.com>
2024-07-29 02:25:36 +0600

    Contact info

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,5 +1,27 @@
+"use client"
+
+import { motion } from "framer-motion";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+
+
+import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "@/components/ui/select";
+import {FaPhoneAlt, FaEnvelope, FaMapMarkerAlt} from "react-icons/fa"
+
+
+
 const Contact = () => {
-  return <div>Contact Page</div>;
+  return (<motion.div
+  initial={{ opacity: 0 }}
+  animate={{
+    opacity: 1,
+    transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+  }}
+  className="min-h-[80vh] flex items-center justify-center py-12 xl:py-0"
+>
+  <div>jdfasldjf</div>
+</motion.div>)
 };
 
 export default Contact;
