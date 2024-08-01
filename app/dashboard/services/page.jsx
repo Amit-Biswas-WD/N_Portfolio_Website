f2e635fd9fab22b516b0c5ee55c4761c04ff38ee
unknown <amitbiswas9282@gmail.com>
2024-08-01 23:16:51 +0600

    dashboard services

---
+++ app/dashboard/services/page.jsx
@@ -0,0 +1,41 @@
+"use client"
+
+import { Input } from "@/components/ui/input"
+import { Textarea } from "@/components/ui/textarea"
+import { Button } from "@/components/ui/button";
+
+const services = [
+  {
+    title: <Input name="title" className="w-full" type="text" placeholder="Type title" />,
+    description: <Textarea name="text" className="h-[200px] my-4" placeholder="Type your message here." />
+
+  }
+]
+
+const ServiceInput = () => {
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const title = form.title.value;
+    const text = form.text.value;
+    console.log(title, text);
+  }
+
+  return (
+    <div className="container mx-auto">
+      <form onSubmit={handleSubmit}>
+      {services.map((item, index) => (
+          <div key={index}>
+            <h2>{item.title}</h2>
+            <h2>{item.input}</h2>
+            {item.description}
+          </div>
+        ))}
+        <Button size="md" type="submit" className="max-w-40">Submit</Button>
+      </form>
+    </div>
+  )
+}
+
+export default ServiceInput
