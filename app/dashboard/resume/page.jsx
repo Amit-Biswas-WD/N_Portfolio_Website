a4a15423a5fd69b77a1bb4a950f5ed5e7ddc1982
unknown <amitbiswas9282@gmail.com>
2024-08-02 00:27:28 +0600

    DashBoard Resume Education

--- app/dashboard/resume/page.jsx
+++ app/dashboard/resume/page.jsx
@@ -7,104 +7,189 @@ import { Button } from "@/components/ui/button";
 const experience = [
   {
     title: "Experience",
-    input: (
-      <>
-        <Input
-          name="experience"
-          className="w-full my-4"
-          type="text"
-          placeholder="Type experience"
-        />
-      </>
+    items: [
+      {
+        company: (
+          <Input
+            name="company"
+            className="w-full my-4"
+            type="text"
+            placeholder="Type company"
+          />
+        ),
+        position: (
+          <Input
+            name="position"
+            className="w-full my-4"
+            type="text"
+            placeholder="Type position"
+          />
+        ),
+        duration: (
+          <Input
+            name="duration"
+            className="w-full my-4"
+            type="text"
+            placeholder="Type duration"
+          />
+        ),
+      },
+    ],
+    description: (
+      <Textarea
+        name="experienceText"
+        className="h-[200px] my-4"
+        placeholder="Type your message here."
+      />
     ),
-    description: <Textarea
-    name="experienceText"
-    className="h-[200px] my-4"
-    placeholder="Type your message here."
-  />
   },
 ];
-
-
-const experiences = {
-  title: "My experience",
-  description:
-    "Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. ",
-  items: [
-    {
-      company: "Learning",
-      position: "Front-end Web Developer / MERN Stack Developer",
-      duration: "2023 - present",
-    },
-    {
-      company: "Programming Hero",
-      position: "Front-end Web Developer / MERN Stack Developer Course",
-      duration: "2023-12-20 - present",
-    },
-  ],
-};
-
 
 const education = [
   {
     title: "Education",
-    input: (
-      <>
-        <Input
-          name="education"
-          className="w-full my-4"
-          type="text"
-          placeholder="Type education"
-        />
-      </>
+    items: [
+      {
+        institution: (
+          <Input
+            name="institution"
+            className="w-full my-4"
+            type="text"
+            placeholder="Type institution"
+          />
+        ),
+        degree: (
+          <Input
+            name="degree"
+            className="w-full my-4"
+            type="text"
+            placeholder="Type degree"
+          />
+        ),
+        duration: (
+          <Input
+            name="duration"
+            className="w-full my-4"
+            type="text"
+            placeholder="Type duration"
+          />
+        ),
+      },
+    ],
+    description: (
+      <Textarea
+        name="educationText"
+        className="h-[200px] my-4"
+        placeholder="Type your message here."
+      />
     ),
-    description: <Textarea
-    name="educationText"
-    className="h-[200px] my-4"
-    placeholder="Type your message here."
-  />
   },
 ];
 
+
 const skills = [
   {
-    title: "Skills",
-    input: (
-      <>
-        <Input
-          name="skills"
-          className="w-full my-4"
-          type="text"
-          placeholder="Type skills"
-        />
-      </>
+    title: "My Skills",
+    skillList: [
+      {
+        icon: (
+          <Input
+            name="icon"
+            className="w-full my-4"
+            type="text"
+            placeholder="Type icon"
+          />
+        ),
+        name: (
+          <Input
+            name="name"
+            className="w-full my-4"
+            type="text"
+            placeholder="Type name"
+          />
+        ),
+      },
+    ],
+    description: (
+      <Textarea
+        name="skillsText"
+        className="h-[200px] my-4"
+        placeholder="Type your message here."
+      />
     ),
-    description: <Textarea
-    name="skillsText"
-    className="h-[200px] my-4"
-    placeholder="Type your message here."
-  />
   },
 ];
 
 const about = [
   {
     title: "About",
-    input: (
-      <>
-        <Input
-          name="about"
-          className="w-full my-4"
-          type="text"
-          placeholder="Type about"
-        />
-      </>
+    info: [
+      {
+        name: (
+          <Input
+            name="name"
+            className="w-full my-4"
+            type="text"
+            placeholder="Type name"
+          />
+        ),
+        phone: (
+          <Input
+            name="phone"
+            className="w-full my-4"
+            type="text"
+            placeholder="Type phone"
+          />
+        ),
+        experience: (
+          <Input
+            name="experience"
+            className="w-full my-4"
+            type="text"
+            placeholder="Type experience"
+          />
+        ),
+        nationality: (
+          <Input
+            name="nationality"
+            className="w-full my-4"
+            type="text"
+            placeholder="Type nationality"
+          />
+        ),
+        email: (
+          <Input
+            name="email"
+            className="w-full my-4"
+            type="text"
+            placeholder="Type email"
+          />
+        ),
+        language: (
+          <Input
+            name="language"
+            className="w-full my-4"
+            type="text"
+            placeholder="Type language"
+          />
+        ),
+        webdev: (
+          <Input
+            name="webdev"
+            className="w-full my-4"
+            type="text"
+            placeholder="Type Web Development"
+          />
+        ),
+      },
+    ],
+    description: (
+      <Textarea
+        name="aboutText"
+        className="h-[200px] my-4"
+        placeholder="Type your message here."
+      />
     ),
-    description:  <Textarea
-    name="aboutText"
-    className="h-[200px] my-4"
-    placeholder="Type your message here."
-  />
   },
 ];
 
@@ -120,31 +205,34 @@ const ResumeInput = () => {
   return (
     <div className="container mx-auto">
       <form onSubmit={handleSubmit}>
-        {about.map((item, index) => (
-          <div key={index}>
-            <h2>{item.title}</h2>
-            <h2>{item.input}</h2>
-            {item.description}
-          </div>
-        ))}
-        {skills.map((item, index) => (
-          <div key={index}>
-            <h2>{item.title}</h2>
-            <h2>{item.input}</h2>
-            {item.description}
-          </div>
-        ))}
-        {education.map((item, index) => (
-          <div key={index}>
-            <h2>{item.title}</h2>
-            <h2>{item.input}</h2>
-            {item.description}
-          </div>
-        ))}
         {experience.map((item, index) => (
           <div key={index}>
             <h2>{item.title}</h2>
-            <h2>{item.input}</h2>
+            {experience.items.map((subItem, subIndex) => (
+              <div key={subIndex}>
+                {subItem.company}
+                {subItem.position}
+                {subItem.duration}
+              </div>
+            ))}
+            {item.description}
+          </div>
+        ))}
+        <Button size="md" type="submit" className="max-w-40 my-4">
+          Submit
+        </Button>
+      </form>
+      <form onSubmit={handleSubmit}>
+        {education.map((item, index) => (
+          <div key={index}>
+            <h2>{item.title}</h2>
+            {item.items.map((subItem, subIndex) => (
+              <div key={subIndex}>
+                {subItem.institution}
+                {subItem.degree}
+                {subItem.duration}
+              </div>
+            ))}
             {item.description}
           </div>
         ))}
