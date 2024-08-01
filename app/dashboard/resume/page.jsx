150165ef286fb2a2c365220e631a458ea274c9ef
unknown <amitbiswas9282@gmail.com>
2024-08-01 23:16:13 +0600

    dashboard work

--- app/dashboard/resume/page.jsx
+++ app/dashboard/resume/page.jsx
@@ -4,53 +4,150 @@ import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
 
+const experience = [
+  {
+    title: "Experience",
+    input: (
+      <>
+        <Input
+          name="experience"
+          className="w-full my-4"
+          type="text"
+          placeholder="Type experience"
+        />
+      </>
+    ),
+    description: <Textarea
+    name="experienceText"
+    className="h-[200px] my-4"
+    placeholder="Type your message here."
+  />
+  },
+];
+
+
+const experiences = {
+  title: "My experience",
+  description:
+    "Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. ",
+  items: [
+    {
+      company: "Learning",
+      position: "Front-end Web Developer / MERN Stack Developer",
+      duration: "2023 - present",
+    },
+    {
+      company: "Programming Hero",
+      position: "Front-end Web Developer / MERN Stack Developer Course",
+      duration: "2023-12-20 - present",
+    },
+  ],
+};
+
+
+const education = [
+  {
+    title: "Education",
+    input: (
+      <>
+        <Input
+          name="education"
+          className="w-full my-4"
+          type="text"
+          placeholder="Type education"
+        />
+      </>
+    ),
+    description: <Textarea
+    name="educationText"
+    className="h-[200px] my-4"
+    placeholder="Type your message here."
+  />
+  },
+];
+
+const skills = [
+  {
+    title: "Skills",
+    input: (
+      <>
+        <Input
+          name="skills"
+          className="w-full my-4"
+          type="text"
+          placeholder="Type skills"
+        />
+      </>
+    ),
+    description: <Textarea
+    name="skillsText"
+    className="h-[200px] my-4"
+    placeholder="Type your message here."
+  />
+  },
+];
+
+const about = [
+  {
+    title: "About",
+    input: (
+      <>
+        <Input
+          name="about"
+          className="w-full my-4"
+          type="text"
+          placeholder="Type about"
+        />
+      </>
+    ),
+    description:  <Textarea
+    name="aboutText"
+    className="h-[200px] my-4"
+    placeholder="Type your message here."
+  />
+  },
+];
+
 const ResumeInput = () => {
   const handleSubmit = (event) => {
     event.preventDefault();
     const form = event.target;
-    const title = form.title.value;
-    const text = form.text.value;
-    console.log(title, text);
+    const formData = new FormData(form);
+    const data = Object.fromEntries(formData.entries());
+    console.log(data);
   };
 
   return (
     <div className="container mx-auto">
       <form onSubmit={handleSubmit}>
-        <Input
-          name="title"
-          className="w-full my-4"
-          type="text"
-          placeholder="Type title"
-        />
-        <Input
-          name="title"
-          className="w-full my-4"
-          type="text"
-          placeholder="Type title"
-        />
-        <Input
-          name="title"
-          className="w-full my-4"
-          type="text"
-          placeholder="Type title"
-        />
-        <Input
-          name="title"
-          className="w-full my-4"
-          type="text"
-          placeholder="Type title"
-        />
-        <Input
-          name="title"
-          className="w-full my-4"
-          type="text"
-          placeholder="Type title"
-        />
-        <Textarea
-          name="text"
-          className="h-[200px] my-4"
-          placeholder="Type your message here."
-        />
+        {about.map((item, index) => (
+          <div key={index}>
+            <h2>{item.title}</h2>
+            <h2>{item.input}</h2>
+            {item.description}
+          </div>
+        ))}
+        {skills.map((item, index) => (
+          <div key={index}>
+            <h2>{item.title}</h2>
+            <h2>{item.input}</h2>
+            {item.description}
+          </div>
+        ))}
+        {education.map((item, index) => (
+          <div key={index}>
+            <h2>{item.title}</h2>
+            <h2>{item.input}</h2>
+            {item.description}
+          </div>
+        ))}
+        {experience.map((item, index) => (
+          <div key={index}>
+            <h2>{item.title}</h2>
+            <h2>{item.input}</h2>
+            {item.description}
+          </div>
+        ))}
         <Button size="md" type="submit" className="max-w-40 my-4">
           Submit
         </Button>
--- app/dashboard/work/page.jsx
+++ app/dashboard/work/page.jsx
@@ -4,6 +4,18 @@ import { Input } from "@/components/ui/input"
 import { Textarea } from "@/components/ui/textarea"
 import { Button } from "@/components/ui/button";
 
+const work = [
+  {
+    title: <Input name="title" className="w-full my-4" type="text" placeholder="Type title" />,
+    technology: <Input name="technology" className="w-full my-4" type="text" placeholder="Type Technology" />,
+    live: <Input name="live" className="w-full my-4" type="text" placeholder="Type Live Link" />,
+    github: <Input name="github" className="w-full my-4" type="text" placeholder="Type Github Link" />,
+    image: <Input name="image" className="w-full my-4" type="text" placeholder="Type Image" />,
+    description: <Textarea name="text" className="h-[200px] my-4" placeholder="Type your message here." />
+
+  }
+]
+
 const WorkInput = () => {
 
   const handleSubmit = (event) => {
@@ -21,12 +33,16 @@ const WorkInput = () => {
   return (
     <div className="container mx-auto">
       <form onSubmit={handleSubmit}>
-        <Input name="title" className="w-full my-4" type="text" placeholder="Type title" />
-        <Input name="technology" className="w-full my-4" type="text" placeholder="Type Technology" />
-        <Input name="live" className="w-full my-4" type="text" placeholder="Type Live Link" />
-        <Input name="github" className="w-full my-4" type="text" placeholder="Type Github Link" />
-        <Input name="image" className="w-full my-4" type="text" placeholder="Type Image" />
-        <Textarea name="text" className="h-[200px] my-4" placeholder="Type your message here." />
+      {work.map((item, index) => (
+          <div key={index}>
+            <h2>{item.title}</h2>
+            <p>{item.technology}</p>
+            <p>{item.live}</p>
+            <p>{item.github}</p>
+            <p>{item.image}</p>
+            {item.description}
+          </div>
+        ))}
         <Button size="md" type="submit" className="max-w-40 my-4">Submit</Button>
       </form>
     </div>
